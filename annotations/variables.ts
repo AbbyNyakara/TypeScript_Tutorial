bd9df9833f88e72473ecbfb63bf9b91cb2e9f58b
Abigael Nyakara <mogusuab@msu.edu>
2024-05-06 13:07:30 -0400

    initial commit

---
+++ annotations/variables.ts
@@ -0,0 +1,29 @@
+const apples:number = 5;
+let speed:string = "fast";
+let hasMuch:boolean = true;
+let nothing:null = null;
+let oops: undefined = undefined;
+
+// built in objectsL 
+
+let now:Date = new Date();
+
+// Group ones: 
+//Arrays
+let colors:string[] = ['red', 'orange', 'yellow']
+let myNumbers: number[] = [1, 2, 4]
+
+
+// Classes: 
+class Car {
+
+}
+
+let car: Car = new Car()
+
+// Object Literal: 
+
+let point: {x: number; y:number}= {
+  x: 10, 
+  y:20
+}
